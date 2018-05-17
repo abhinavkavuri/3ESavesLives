2c7c4d94d6399820d6287fa4978c80b1ae5f3043
kavuri abhinav <abhinavkavuri@gmail.com>
2018-05-17 13:00:24 +0530

    Add files via upload

---
+++ front page 1/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>3ESavesLife</title>
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Titillium+Web:900" rel="stylesheet">
+  
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  <nav class='main hList inactive'>
+  <div class="tab active"><span>Engineering</span></div><!--
+  --><div class="tab"><span>Education</span></div><!--
+  --><div class="tab"><span>Enforcement</span></div><!--
+  -->
+</nav>
+<page id="home">
+  <span class="pagetag">#3M</span>
+  <section class="header">
+    <div>
+      <h2><span>3E </span><span> SavesLife</span></h2>
+      <h5>Prototype 1</h5>
+    </div>
+    <div class='logo'></div>
+  </section>
+  <section id="grid_wrapper" class="init">
+    <card>
+      <div>
+        <span>ENGINEERING</span>
+      </div>
+    </card>
+    <card>
+      <div>
+        <span>EDUCATION</span>
+      </div>
+    </card>
+    <card>
+      <div>
+        <span>ENFORCEMENT</span>
+      </div>
+    </card>
+    
+    
+  </section>
+</page>
+<div id="content_wrapper" class='inactive'>
+  <button class="close">&#xf00d;</button>
+  <div class='content'>
+    <section>
+      <h2>#1</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+    <section>
+      <h2>#2</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+    <section>
+      <h2>#3</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+    <section>
+      <h2>#4</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+    <section>
+      <h2>#5</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+    <section>
+      <h2>#6</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+    <section>
+      <h2>#7</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+    <section>
+      <h2>#8</h2>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+      <p>
+        Here's some random shit for this random tab on this random template.
+      </p>
+    </section>
+  </div>
+</div>
+<div id="footer">
+  <span style="display:block;font-size:0.9em;line-height:55px;"><span style="display:inline-block;">&copy; <span style="font-family:'Titillium Web';font-weight:900;letter-spacing:0.06em">Designed by ABHINAV</span>2018</span></span>
+  <span style="display:block;font-size:0.7em;line-height:30px;margin-top:-25px;color:rgba(255,255,255,0.15)">&lang; !--Jus kidding-- &rang;<span>
+</div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.4/SmoothScroll.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js'></script>
+
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
